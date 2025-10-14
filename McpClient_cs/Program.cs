9cc75cbde41923ea1a3350c80bb5622fad208006
waynen12 <waynen4@gmail.com>
2025-10-14 20:12:08 +0100

    Initial commit. MCP client solution

---
+++ McpClient_cs/Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.AI;
+using OpenAI;
+
+namespace McpClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
